89b38d1b581681050005ee0d713965e816107051
ibrahim <ibrahim.asif.mirza@gmail.com>
2021-01-31 00:01:02 +0100

    💄 style(app):  Style basic layouts

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,14 +3,18 @@
 import { NgModule } from '@angular/core';
 import { MainEditorPageComponent } from './main-editor-page/main-editor-page.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { TextEditorComponent } from './main-editor-page/components/text-editor/text-editor.component';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
   declarations: [
-    MainEditorPageComponent
+    MainEditorPageComponent,
+    TextEditorComponent
   ],
   imports: [
-      MatSidenavModule
+      MatSidenavModule,
+      MatInputModule
   ],
 })
 export class PagesModule { }
