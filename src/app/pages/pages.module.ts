d25acf2d8b9caf07ac02a75c466124f43024fbec
ibrahim <ibrahim.asif.mirza@gmail.com>
2021-02-01 21:47:36 +0100

    refactor(pages.module): Add mat modules

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,14 +5,12 @@ import { MainEditorPageComponent } from './main-editor-page/main-editor-page.com
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { TextEditorComponent } from './main-editor-page/components/text-editor/text-editor.component';
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule, MatFormFieldModule, MatIconModule, MatSelectModule, MatTabsModule } from '@angular/material';
+import { MatButtonModule, MatDialogModule, MatFormFieldModule, MatIconModule, MatMenuModule, MatSelectModule, MatTabsModule } from '@angular/material';
+import { HyperlinkDialogComponent } from './main-editor-page/components/hyperlink-dialog/hyperlink-dialog.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    MainEditorPageComponent,
-    TextEditorComponent
-  ],
   imports: [
       MatSidenavModule,
       MatInputModule,
@@ -20,7 +18,17 @@ import { MatButtonModule, MatFormFieldModule, MatIconModule, MatSelectModule, Ma
       MatButtonModule,
       MatIconModule,
       MatSelectModule,
-      MatFormFieldModule
+      MatFormFieldModule,
+      MatMenuModule,
+      MatDialogModule,
+      MatInputModule,
+      FormsModule
   ],
+  declarations: [
+    MainEditorPageComponent,
+    TextEditorComponent,
+    HyperlinkDialogComponent
+  ],
+  entryComponents: [HyperlinkDialogComponent],
 })
 export class PagesModule { }
