0871ea82d29a5277468cafdec76fe6910d4ec628
ibrahim <ibrahim.asif.mirza@gmail.com>
2021-02-02 01:06:45 +0100

    ✨ feat(main-editor-page): Toggle side menu

--- src/app/pages/main-editor-page/main-editor-page.component.ts
+++ src/app/pages/main-editor-page/main-editor-page.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainEditorPageComponent implements OnInit {
 
+  events: string[] = [];
+  opened: boolean;
+
   constructor() { }
 
   ngOnInit() {
